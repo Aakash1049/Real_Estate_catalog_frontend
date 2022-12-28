f908abff68c7c0bfd4b7f909cb6eb15d4485cac1
Aakash Sangapure <aakashdadasangapure@gmail.com>
2022-12-28 17:11:49 +0530

    aa

--- real_estate_frontend/src/App.js
+++ real_estate_frontend/src/App.js
@@ -4,14 +4,9 @@ import Signup from './Component/Signup/Signup';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signin from './Component/Signin/Signin';
 import SideBar from './Component/Dashboard/SideBar';
-<<<<<<< HEAD
 import GeneralInfo from './Component/General Info/generalInfo';
 import LocationInfo from './Component/locationInfo/locationInfo';
 import Content from "./Component/Dashboard/DashboardContent"
-=======
-import Content from './Component/Dashboard/DashboardContent';
-
->>>>>>> 12f8bf29d8b7a2023fe8d9475d2f5c84fa879ff5
 function App() {
   return (
     <div className="App">
