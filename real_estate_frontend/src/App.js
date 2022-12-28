1c62203a9eae3623e64c6a0a19c6f93cced5526a
Vikas Dangi <vikasdangi675@gmail.com>
2022-12-28 16:07:18 +0530

    COntentBoard component created

--- real_estate_frontend/src/App.js
+++ real_estate_frontend/src/App.js
@@ -4,6 +4,7 @@ import Signup from './Component/Signup/Signup';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signin from './Component/Signin/Signin';
 import SideBar from './Component/Dashboard/SideBar';
+import Content from './Component/Dashboard/DashboardContent';
 
 function App() {
   return (
@@ -12,7 +13,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Signin/>}/>
         <Route path='/Signup' element={<Signup/>}/>
-        <Route path='/dashboard' element={<SideBar/>}/>
+        {/* <Route path='/dashboard' element={<Dashboard/>}/> */}
+        <Route path='/SideBar' element={<SideBar/>}/>
+        <Route path='/Content' element={<Content/>}/>
       </Routes>
       </BrowserRouter>
     
--- real_estate_frontend/src/Component/Dashboard/SideBar.js
+++ real_estate_frontend/src/Component/Dashboard/SideBar.js
@@ -20,30 +20,14 @@ export default function SideBar(){
        </ul>
             </section>
         </div>
-
+        
         
         <div className="middle">
             <nav className="head">
              <h4>USER ID : 12PPD123</h4>   
              <img id="img-2" src={require("../../Images/person.png")} alt="logo"/>
             </nav>
-            <div className="content">
-              <input id="search" type="search" placeholder="Search PPD ID" name="search"    />           
-              <button id="btn">+ Add Property</button>
-              <div className="list">
-                <ul className="ul">
-                    <li className="list-item">PPD ID</li>
-                    <li className="list-item">Image</li>
-                    <li className="list-item">Property</li>
-                    <li className="list-item">Contact</li>
-                    <li className="list-item">Area</li>
-                    <li className="list-item">Views</li>
-                    <li className="list-item">Status</li>
-                    <li className="list-item">DayLift</li>
-                    <li className="list-item">Action</li>
-                </ul>
-              </div>
-               </div>
+          
         </div>
         </div>
         </>
