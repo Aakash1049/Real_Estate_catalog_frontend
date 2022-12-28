fe3278261c22cf9b9714d67ed5752c1d759553f4
Tejas Bhadane <tejasbhadane98@gmail.com>
2022-12-28 16:10:19 +0530

    Basic Information Added

--- real_estate_frontend/src/App.js
+++ real_estate_frontend/src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 import Signup from './Component/Signup/Signup';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signin from './Component/Signin/Signin';
-import SideBar from './Component/Dashboard/SideBar';
+import SideBar from './Component/Dashboard/SideBar'
+import Basic  from './Forms/Basicinfo';
+
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
         <Route path='/' element={<Signin/>}/>
         <Route path='/Signup' element={<Signup/>}/>
         <Route path='/d' element={<SideBar/>}/>
+        <Route path='/Basicinfo' element={<Basic/>}/>    
       </Routes>
       </BrowserRouter>
     
