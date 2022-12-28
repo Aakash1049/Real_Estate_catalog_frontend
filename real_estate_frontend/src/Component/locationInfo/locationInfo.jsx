6a371741b891ef079978e414e10cbc301db97836
Aakash Sangapure <aakashdadasangapure@gmail.com>
2022-12-28 19:53:51 +0530

    location info update

--- real_estate_frontend/src/Component/locationInfo/locationInfo.jsx
+++ real_estate_frontend/src/Component/locationInfo/locationInfo.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 import "./locationInfo.css"
 import SideBar from '../Dashboard/SideBar'
 import PropertyNavigation from '../PropertyNavigation/PropertyNavigation'
+import { useState } from 'react'
 const LocationInfo = () => {
-    
-
-
+    const [email,setEmail]=useState("")
+    const [city,,setCity]=useState("")
+    const [area,setArea]=useState("")
+    const [pincode, setPincode]=useState()
+    const [address,setAdress]=useState("")
+    const [landmark,setLandmark]=useState("")
+    const [lattitude, setLattitude]=useState()
+    const [longitude, setLongitude]=useState()
 
     return (
         <>
@@ -16,24 +22,24 @@ const LocationInfo = () => {
                     <div>
                         <label htmlFor="email">Email</label>
                         <br />
-                        <input type="email" id="email" /> <br />
+                        <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} id="email" /> <br />
                     </div>
                     <div>
                         <label htmlFor="city">City</label>
                         <br />
-                        <input type="text" id="city" placeholder='Enter your city' /> <br />
+                        <input type="text" value={city} onChange={(e)=>setCity(e.target.value)}  id="city" placeholder='Enter your city' /> <br />
                     </div>
                 </div>
                 <div className='a'>
                     <div>
                         <label htmlFor="area">Area</label>
                         <br />
-                        <input type="text" id="area" /> <br />
+                        <input type="text" value={area} onChange={(e)=>setArea(e.target.value)}  id="area" /> <br />
                     </div>
                     <div>
                         <label htmlFor="pincode">Pincode</label>
                         <br />
-                        <input type="number" id="city" placeholder='Enter your Pincode' /> <br />
+                        <input type="number" value={pincode} onChange={(e)=>setPincode(e.target.value)}  id="city" placeholder='Enter your Pincode' /> <br />
                     </div>
 
                 </div>
@@ -41,24 +47,24 @@ const LocationInfo = () => {
                     <div>
                         <label htmlFor="address">Address</label>
                         <br />
-                        <input type="text" id="address" /> <br />
+                        <input type="text" value={address} onChange={(e)=>setAdress(e.target.value)}  id="address" /> <br />
                     </div>
                     <div>
                         <label htmlFor="landmark">Landmark</label>
                         <br />
-                        <input type="text" id="landmark" />
+                        <input type="text" value={landmark} onChange={(e)=>setLandmark(e.target.value)}  id="landmark" />
                     </div>
                 </div>
                 <div className='a'>
                     <div>
                         <label htmlFor="lattitude">lattitude</label>
                         <br />
-                        <input type="number" id="lattitude" /> <br />
+                        <input type="number" value={lattitude} onChange={(e)=>setLattitude(e.target.value)}  id="lattitude" /> <br />
                     </div>
                     <div>
                         <label htmlFor="longitude">longitude</label>
                         <br />
-                        <input type="number" id="longitude" />
+                        <input type="number" value={longitude} onChange={(e)=>setLongitude(e.target.value)}  id="longitude" />
                     </div>
                 </div>
                 <div className="buttons">
