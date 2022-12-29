48739397e6d8dbe070dc27c2d32ee41a342c4eb6
Tejas Bhadane <tejasbhadane98@gmail.com>
2022-12-29 14:48:10 +0530

    File Updated

--- real_estate_frontend/src/Component/Dashboard/DashboardContent.js
+++ real_estate_frontend/src/Component/Dashboard/DashboardContent.js
@@ -1,16 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Dashboard/DashBoardContent.css"
 import SideBar from "./SideBar";
 import { useNavigate } from "react-router-dom";
 
 export default function Content() {
-    const navigate =useNavigate()
+    const navigate =useNavigate();
     return (
         <>
         <SideBar/>
          <div className="content">
               <input id="search" type="text" placeholder="Search PPD ID" name="search"    />           
-              <button id="btn">+ Add Property</button>
+              <button id="btn" onClick={()=>navigate("/Basicinfo")}>+ Add Property</button>
              
 
                 <ul className="ul">
