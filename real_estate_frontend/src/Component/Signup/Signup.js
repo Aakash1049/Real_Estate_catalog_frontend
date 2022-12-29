3d9953a73a62e5ab4b84ee80822ab95326a836f6
Aakash Sangapure <aakashdadasangapure@gmail.com>
2022-12-29 13:00:53 +0530

    backend connected

--- real_estate_frontend/src/Component/Signup/Signup.js
+++ real_estate_frontend/src/Component/Signup/Signup.js
@@ -1,46 +1,63 @@
 import React, { useEffect, useState } from 'react';
 import "../Signup/Signup.css"
 import { Link } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
-export default function Signup(){
+export default function Signup() {
 
-     const [email,setEmail] = useState("")
-     const [password,setPassword] = useState("")
-     const [confirmPassword,setConfirmPassword]  = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+    const navigate = useNavigate()
 
-
-const SignUpHandler = ()=>{
-    fetch("http://localhost:8000/signUp")
-}
-   
-
-
+    const SignUpHandler = (e) => {
+        e.preventDefault()
+        console.log(email, password, confirmPassword)
+        fetch("/signUp", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({
+                email, password, confirmpassword: confirmPassword
+            })
+        }).then(res => res.json())
+            .then(data => {
+                if (data.error) {
+                    alert(data.error)
+                }
+                else {
+                    alert(data.message)
+                    navigate("/")
+                }
+            })
+    }
 
     return (
         <>
-        <form>
-        <div className='main'>
-      <section className='logo'>
-       <h1>Logo</h1>
-        </section>  
-        <h2>Create  New Account</h2> 
+            <form>
+                <div className='main'>
+                    <section className='logo'>
+                        <h1>Logo</h1>
+                    </section>
+                    <h2>Create  New Account</h2>
 
-        <div className='email'>
-            <input id="em" type="email"  placeholder='Mail Id' name='email'/>
-            </div>  
-            <div className='pwd'>
-            <input  id="pw" type="password"  placeholder="password"  name='password'/>
-            </div> 
-            <div className=' con-pwd'>
-            <input  id="con-pw" type="password"  placeholder=" Confirm password"  name=' confirm password'/>
-            </div> 
-          
-            <div className='btn'>
-                <button id="bt" onClick={SignUpHandler}>Sign Up</button>
-            </div>
-          </div>
-          <Link to="/"><a href="Signin.js" >Sign in</a></Link> 
-          </form>
+                    <div className='email'>
+                        <input id="em" type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Mail Id' name='email' />
+                    </div>
+                    <div className='pwd'>
+                        <input id="pw" type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="password" name='password' />
+                    </div>
+                    <div className=' con-pwd'>
+                        <input id="con-pw" type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} placeholder=" Confirm password" name=' confirm password' />
+                    </div>
+
+                    <div className='btn'>
+                        <button id="bt" onClick={(e) => SignUpHandler(e)}>Sign Up</button>
+                    </div>
+                </div>
+                <Link to="/">Sign in</Link>
+            </form>
         </>
     )
 }
